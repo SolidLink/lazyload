c45cf5d48e8a6f10df5466d779f5cde68af0f779
git <angelabelgarcia@gmail.com>
2014-10-09 10:51:06 -0700

    rm

--- lazyload.js
+++ lazyload.js
@@ -1,5 +1,4 @@
-/* lazyload.js (c) Lorenzo Giuliani
- * MIT License (http://www.opensource.org/licenses/mit-license.html)
+/*
  *
  * expects a list of:  
  * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
